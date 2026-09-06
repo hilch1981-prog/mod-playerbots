9d7eb608c5eb1c3c153e766eb49b39cd10592586
Hobbyist <hilch1981@gmail.com>
2026-09-06 10:01:41 +0900

    feat(chipa): stage native MoP emote packet reader

--- src/chipa/Mop548UnitPacketCompat.h
+++ src/chipa/Mop548UnitPacketCompat.h
@@ -15,6 +15,17 @@ void ReadDismount(Packet& packet, Guid& guid)
     packet.ReadGuidMask(guid, 6, 3, 0, 7, 1, 2, 5, 4);
     packet.ReadGuidBytes(guid, 3, 6, 7, 5, 1, 4, 2, 0);
 }
+
+// MoP 5.4.8 SMSG_EMOTE is not packed-GUID encoded in this runtime. Unit.cpp
+// writes uint32 emote id followed by the raw uint64 object GUID. Keep parsing
+// isolated here so the donor's modern ObjectGuid::IsPlayer() assumption can be
+// replaced at integration time with the target-native IS_PLAYER_GUID(uint64)
+// predicate from ObjectDefines.h.
+template <class Packet, class EmoteId, class Guid>
+void ReadEmote(Packet& packet, EmoteId& emoteId, Guid& guid)
+{
+    packet >> emoteId >> guid;
+}
 }
 }
 
